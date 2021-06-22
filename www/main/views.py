e41afe32622585f796ac5be548496f1204aeed27
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-06-22 16:55:12 -0400

    added formatting

--- www/main/views.py
+++ www/main/views.py
@@ -7,22 +7,29 @@ from .forms import FightForm
 # Create your views here.
 class Indexview(ListView):
     model = Fight
-    template_name = 'main/index.html'
-    
+    template_name = "main/index.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = {}
+        context["object_list"] = Fight.objects.order_by("-id")
+        return context
+
+
 def new_fight(req):
     if req.user.is_authenticated:
-        if req.method == 'POST':
+        if req.method == "POST":
             form = FightForm(req.POST or None)
             inst = form.save(commit=False)
             inst.op = req.user
             inst.save()
-            return django.http.HttpResponseRedirect('/')
+            return django.http.HttpResponseRedirect("/")
         else:
             form = FightForm()
-            return render(req, 'main/newfight.html', {'form': form})
+            return render(req, "main/newfight.html", {"form": form})
     else:
         return django.http.HttpResponseForbidden
-    
+
+
 def view_fight(req, pk):
     fight = Fight.objects.get(pk=pk)
-    return render(req, 'main/fight.html', {'fight': fight})
+    return render(req, "main/fight.html", {"fight": fight})
